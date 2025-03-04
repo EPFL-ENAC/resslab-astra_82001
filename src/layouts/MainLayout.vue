49801b37b4cf9e01ba94444c1394ad20381f34bb
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-03-04 10:38:24 +0100

    feat: add annex PDF and SVG download links in MainLayout #53

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -45,6 +45,26 @@
         <q-btn
           flat
           round
+          :href="linksToAstraAnnex82001fPDF[lang]"
+          target="_blank"
+          download
+          class="q-ml-md"
+        >
+          <template #default>
+            <q-img
+              height="24px"
+              fit="contain"
+              src="/epfl-designed/Canard/svg/c-annex.svg"
+              :alt="$t('i13')"
+            />
+            <q-tooltip class="text-body1">
+              <span>{{ $t('i13') }} </span>
+            </q-tooltip>
+          </template>
+        </q-btn>
+        <q-btn
+          flat
+          round
           :href="linkToCSV"
           target="_blank"
           :download="downloadFileName"
@@ -101,6 +121,13 @@ const linksToAstra82001fPDF: Record<string, string> = {
   de: 'https://www.astra.admin.ch/dam/astra/de/dokumente/standards_fuer_nationalstrassen/astra_82001_ueberpruefungbestehenderstrassenbruecken2006.pdf.download.pdf/astra_82001f.pdf',
   fr: 'https://www.astra.admin.ch/dam/astra/fr/dokumente/standards_fuer_nationalstrassen/astra_82001_ueberpruefungbestehenderstrassenbruecken2006.pdf.download.pdf/astra_82001f.pdf',
   it: 'https://www.astra.admin.ch/dam/astra/fr/dokumente/standards_fuer_nationalstrassen/astra_82001_ueberpruefungbestehenderstrassenbruecken2006.pdf.download.pdf/astra_82001f.pdf',
+};
+
+const linksToAstraAnnex82001fPDF: Record<string, string> = {
+  en: '/Documentation.Annexe.n.306891-2.2024.V1.03.pdf',
+  de: '/Documentation.Annexe.n.306891-2.2024.V1.03.pdf',
+  fr: '/Documentation.Annexe.n.306891-2.2024.V1.03.pdf',
+  it: '/Documentation.Annexe.n.306891-2.2024.V1.03.pdf',
 };
 </script>
 
